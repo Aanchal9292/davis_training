d35af9c502f8c758fa77315a0862c3d7be6b9412
Aanchal9292 <aanchal9280@gmail.com>
2026-03-17 11:19:03 +0530

    Addition

---
+++ Class_Work/additionofnumbers.py
@@ -0,0 +1,7 @@
+# function to add two numbers
+def addnumbers(x,y):
+    return x+y
+# main program
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+print("Sum : ",addnumbers(num1,num2))
