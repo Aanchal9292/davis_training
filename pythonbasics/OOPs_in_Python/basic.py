c18921a79ffbe9ea9392826000e663df8c27a780
Aanchal9292 <aanchal9280@gmail.com>
2026-04-02 21:02:14 +0530

    Basic Of Class

---
+++ pythonbasics/OOPs_in_Python/basic.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self,engine,mileage):
+        self.engine = engine
+        self.mileage = mileage
+    def info(self):
+        print(f"Engine : {self.engine}")
+        print(f"Mileage : {self.mileage}")
+car1 = Car("4000cc",14)
+car1.info()
+car2 = Car("1200cc",23.9)
+car2.info()
