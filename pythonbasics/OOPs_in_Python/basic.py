b5e30a68d0ff73e1848df20210da0cb31ddc1c3c
Aanchal9292 <aanchal9280@gmail.com>
2026-04-03 09:24:20 +0530

    Information of Car

--- pythonbasics/OOPs_in_Python/basic.py
+++ pythonbasics/OOPs_in_Python/basic.py
@@ -1,11 +1,17 @@
 class Car:
-    def __init__(self,engine,mileage):     #special method
+    def __init__(self,brand,model,color,engine,mileage):     #special method
+        self.brand = brand
+        self.model = model
+        self.color = color
         self.engine = engine
         self.mileage = mileage
     def info(self):        # self is mandatory
+        print(f"Model : {self.model}")
+        print(f"Engine : {self.engine}")
+        print(f"Color : {self.color}")
         print(f"Engine : {self.engine}")
         print(f"Mileage : {self.mileage}")
-car1 = Car("4000cc",14)
+car1 = Car("Maruti Suzuki","Swift","Red","1197cc",25)
 car1.info()
-car2 = Car("1200cc",23.9)
+car2 = Car("Hyundai","Creta","Black","2000cc",21)
 car2.info()
