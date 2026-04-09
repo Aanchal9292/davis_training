32677985a0407d3172365e475726e51da866e69d
Aanchal9292 <aanchal9280@gmail.com>
2026-04-09 09:59:56 +0530

    Finding factorial of number

---
+++ Python_Coding_Challenge-2/factorial.py
@@ -0,0 +1,11 @@
+# Function to calculate factorial
+def factorial(n):
+    fact = 1
+
+    for i in range(1, n+1):
+        fact *= i
+
+    return fact
+
+n = int(input("Enter the number : "))
+print(factorial(n))
