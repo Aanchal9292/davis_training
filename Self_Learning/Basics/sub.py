450c96e0b707ddfba670c17644956928691d1988
Aanchal9292 <aanchal9280@gmail.com>
2026-04-17 15:38:35 +0530

    Subtraction of two numbers

---
+++ Self_Learning/Basics/sub.py
@@ -0,0 +1,7 @@
+# function to add two numbers
+def subnumbers(x,y):
+    return x-y
+# main program
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+print("Sum : ",subnumbers(num1,num2))
