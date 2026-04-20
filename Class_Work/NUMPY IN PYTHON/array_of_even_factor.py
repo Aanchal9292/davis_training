8f892992f1b30b0abb759f6e1e99590cff13f9c5
Aanchal9292 <aanchal9280@gmail.com>
2026-04-20 10:52:35 +0530

    Array of even factors of a number

--- Class_Work/NUMPY IN PYTHON/array_of_even_factor.py
+++ Class_Work/NUMPY IN PYTHON/array_of_even_factor.py
@@ -1,19 +1,29 @@
 import numpy as np
 
-# Input from user
+# Input
 num = int(input("Enter a number: "))
 
-even_factors = []
+# Conditions
+if num == 0:
+    print("Zero does not have finite factors. Please enter a non-zero number.")
 
-# Finding factors
-for i in range(1, num + 1):
-    if num % i == 0:      # check factor
-        if i % 2 == 0:    # check even
+elif num < 0:
+    print("Negative numbers are not allowed. Please enter a positive number.")
+
+elif num % 2 != 0:
+    print("The number is odd, so it has no even factors.")
+
+else:
+    even_factors = []
+
+    # Finding factors
+    for i in range(1, num + 1):
+        if num % i == 0 and i % 2 == 0:
             even_factors.append(i)
 
-# Creating NumPy array
-arr = np.array(even_factors)
+    # Creating NumPy array
+    arr = np.array(even_factors)
 
-# Output
-print("Even factors:", even_factors)
-print("NumPy Array:", arr)
+    # Output
+    print("Even factors:", even_factors)
+    print("NumPy Array:", arr)
