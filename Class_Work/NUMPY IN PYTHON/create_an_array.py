db84d4b02a133a6febb3ec4ed90d68bbfa9c9927
Aanchal9292 <aanchal9280@gmail.com>
2026-04-20 09:50:47 +0530

    Creating an array

--- Class_Work/NUMPY IN PYTHON/create_an_array.py
+++ Class_Work/NUMPY IN PYTHON/create_an_array.py
@@ -1,4 +1,6 @@
 import numpy as np
 # tocreate an array
-numbers = np.array([20,30,40,70,90])
+# numbers = np.array([20,30,40,70,90])
+# print(numbers)
+numbers = np.array([20,30,4.5,70,90])
 print(numbers)
